f17270a4dff202fdf2b676f2f8fd87da96b5f109
yuxianGG <940114763@qq.com>
2019-09-27 20:04:48 +0800

    轮播图完成

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,2 +1,25 @@
 
-Page({})
+// 首页
+
+Page({
+    data:{
+        //轮播图数组
+        swiperList:[]
+    },
+
+    onLoad(){
+        this.getSwiperData()
+    },
+
+    //获取轮播图数据
+    getSwiperData(){
+        wx.request({
+            url:'https://api.zbztb.cn/api/public/v1/home/swiperdata',
+            success:(result) =>{
+                this.setData({
+                    swiperList:result.data.message
+                })
+            }
+        })
+    }
+})
